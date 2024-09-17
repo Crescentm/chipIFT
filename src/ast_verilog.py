f8ec7c0330f1a9b3bcd6a3d33013d9c4b08d1dfd
Crescentm <guyues@outlook.com>
2024-09-17 21:14:08 +0800

    feat: :sparkles: add DFG Flow

--- src/ast_verilog.py
+++ src/ast_verilog.py
@@ -1,6 +1,15 @@
 from pyverilog.vparser.parser import parse
+from pyverilog.dataflow.dataflow_analyzer import VerilogDataflowAnalyzer
 import pyverilog.vparser.ast as vast
 import os
+
+
+class DFGVerilog:
+    def __init__(self, directives, instances, terms, binddict):
+        self.directives = directives
+        self.instances = instances
+        self.terms = terms
+        self.binddict = binddict
 
 
 class ASTVerilog:
@@ -9,9 +18,12 @@ class ASTVerilog:
         self, file_list, include_list: list = [], define_list: list = []
     ) -> None:
         self.file_list: list = file_list
-        self.ast: vast.Source = self.pasrser(file_list, include_list, define_list)
+        self.ast: vast.Source = self._ast_pasrser(file_list, include_list, define_list)
+        self.dfg = self._dfg_parser(
+            file_list, include_list=include_list, define_list=define_list
+        )
 
-    def pasrser(
+    def _ast_pasrser(
         self, file_list: list, include_list: list = [], define_list: list = []
     ) -> vast.Source | None:
 
@@ -30,6 +42,27 @@ class ASTVerilog:
         )
 
         return ast
+
+    def _dfg_parser(
+        self,
+        file_list: list,
+        topmodule: str = "top",
+        noreorder=False,
+        nobind=False,
+        include_list=[],
+        define_list=[],
+    ) -> VerilogDataflowAnalyzer:
+        analyzer = VerilogDataflowAnalyzer(
+            file_list, topmodule, noreorder, nobind, include_list, define_list
+        )
+        analyzer.generate()
+        dfg = DFGVerilog(
+            analyzer.get_directives(),
+            analyzer.getInstances(),
+            analyzer.getTerms(),
+            analyzer.getBinddict(),
+        )
+        return dfg
 
     def decls_add(self, delcs):
         modules = self.ast.children()[0].children()
@@ -52,6 +85,7 @@ def ast_pasrser_test():
     filelist = ["./tests/test.v"]
     ast = ASTVerilog(filelist)
     ast.decls_add(vast.Decl("reg", vast.Identifier("a")))
+    print(ast.dfg.terms.values())
 
 
 if __name__ == "__main__":
