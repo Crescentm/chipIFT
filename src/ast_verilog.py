f19d982a0783650d9df3cea7ecb8b072ec7944c8
Crescentm <guyues@outlook.com>
2024-09-18 11:27:23 +0800

    feat: :sparkles: add Declaration Modify Function

--- src/ast_verilog.py
+++ src/ast_verilog.py
@@ -1,5 +1,9 @@
 from pyverilog.vparser.parser import parse
 from pyverilog.dataflow.dataflow_analyzer import VerilogDataflowAnalyzer
+from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+from pyverilog.dataflow.dataflow_codegen import VerilogCodeGenerator
+from pyverilog.dataflow.optimizer import VerilogDataflowOptimizer
+from enum import Enum
 import pyverilog.dataflow.dataflow as vdfg
 import pyverilog.utils.scope as vscope
 import pyverilog.vparser.ast as vast
@@ -35,6 +39,42 @@ class DFGVerilog:
         self.instances: tuple = analyzer.getInstances()
         self.terms: dict[vscope.ScopeChain, vdfg.Term] = analyzer.getTerms()
         self.binddict: dict[vscope.ScopeChain, list[vdfg.Bind]] = analyzer.getBinddict()
+        self.constlist = analyzer.getConsts()
+
+        self.topmodule = topmodule
+
+    def gen_code(
+        self,
+        clockname: str = "CLK",
+        resetname: str = "RST_X",
+        clockedge: str = "posedge",
+        resetedge: str = "negedge",
+        searchtarget=[],
+    ):
+        # Need to solve
+        # module 'pyverilog.utils.signaltype' has no attribute 'isWireArray'
+        optimizer = VerilogDataflowOptimizer(self.terms, self.binddict)
+        optimizer.resolveConstant()
+        resolved_terms = optimizer.getResolvedTerms()
+        resolved_binddict = optimizer.getResolvedBinddict()
+        constlist = optimizer.getConstlist()
+        codegen = VerilogCodeGenerator(
+            self.topmodule,
+            self.terms,
+            self.binddict,
+            resolved_terms,
+            resolved_binddict,
+            constlist,
+        )
+        codegen.set_clock_info(clockname, clockedge)
+        codegen.set_reset_info(resetname, resetedge)
+        code = codegen.generateCode()
+        return code
+
+
+class DeclType(Enum):
+    REG = 1
+    WIRE = 2
 
 
 class ASTVerilog:
@@ -51,15 +91,59 @@ class ASTVerilog:
             debug=False,
         )
 
-        self.source: vast.Source = ast.children()[0]
+        self.parser = ast
+        self.source: vast.Source = ast
         self.module_num = len(ast.children()[0].children())
-        self.modules: vast.ModuleDef = ast.children()[0].children()
 
-    def decls_add(self, delcs: list[vast.Decl], module: vast.ModuleDef):
-        print(self.modules)
+    def show(self):
+        self.parser.show()
+
+    def new_decl(self, decltype: DeclType, name: list[str], **args) -> vast.Decl:
+        """
+        new_decl: Create one new declaration\n
+        decltype: Type of declaration;
+        name: Name of the declaration, a list of strings;
+        [width]: Width of the declaration, a list of tuples;
+        """
+        decl = []
+        if decltype == DeclType.REG:
+            width: tuple = args["width"]
+            for index in range(len(name)):
+                decl.append(
+                    vast.Reg(
+                        name[index],
+                        width=vast.Width(
+                            vast.IntConst(str(width[0])),
+                            vast.IntConst(str(width[1])),
+                        ),
+                    )
+                )
+            return vast.Decl(tuple(decl))
+
+        elif decltype == DeclType.WIRE:
+            for index in range(len(name)):
+                decl.append(vast.Wire(name[index]))
+
+            return vast.Decl(tuple(decl))
+
+    def add_decls(self, delcs: list[vast.Decl], module_index: int = 0):
+        module: vast.ModuleDef = self.source.description.definitions[module_index]
+        items = tuple(delcs + list(module.items))
+
+        module_new = vast.ModuleDef(
+            module.name, module.paramlist, module.portlist, items
+        )
+
+        module_list = list(self.source.description.definitions)
+        module_list[module_index] = module_new
+        self.source.description.definitions = tuple(module_list)
+
+        return True
 
     def gen_code(self):
-        pass
+        codegen = ASTCodeGenerator()
+        rslt = codegen.visit(self.source)
+        return rslt
 
 
 class VerilogParser:
@@ -76,11 +160,14 @@ class VerilogParser:
         )  # Just ignore the other args
 
 
-def ast_pasrser_test():
+def decl_add_example():
     file_list = ["./tests/test.v"]
-    verilog = VerilogParser(file_list)
-    print(verilog.ast.decls_add([], verilog.ast.modules))
+    ast = ASTVerilog(file_list)
+    decl1 = ast.new_decl(decltype=DeclType.REG, name=["a", "b"], width=(31, 0))
+    decl2 = ast.new_decl(decltype=DeclType.WIRE, name=["a_t", "b_t"], width=(31, 0))
+    ast.add_decls([decl1, decl2])
+    print(ast.gen_code())
 
 
 if __name__ == "__main__":
-    ast_pasrser_test()
+    decl_add_example()
--- tests/test.v
+++ tests/test.v
@@ -1,27 +1,22 @@
-module top
-  (
-   input CLK, 
-   input RST,
-   input enable,
-   input [31:0] value,
-   output [7:0] led
-  );
-  reg [31:0] count;
-  reg [7:0] state;
-  assign led = count[23:16];
-  always @(posedge CLK) begin
-    if(RST) begin
-      count <= 0;
-      state <= 0;
-    end else begin
-      if(state == 0) begin
-        if(enable) state <= 1;
-      end else if(state == 1) begin
-        state <= 2;
-      end else if(state == 2) begin
-        count <= count + value;
-        state <= 0;
-      end
-    end
-  end
+module mux2to1 (
+    input wire a,    // 输入信号 a
+    input wire b,    // 输入信号 b
+    input wire sel,  // 选择信号 sel
+    output wire y    // 输出 y
+);
+    assign y = (sel) ? b : a;
+endmodule
+module adder4bit (
+    input wire [3:0] a,   // 4 位输入 a
+    input wire [3:0] b,   // 4 位输入 b
+    input wire cin,       // 输入进位 cin
+    output wire [3:0] sum, // 4 位和 sum
+    output wire cout      // 输出进位 cout
+);
+    wire [4:0] result;    // 用于存储加法结果的中间变量
+
+    assign result = a + b + cin;
+    assign sum = result[3:0];
+    assign cout = result[4];
+
 endmodule
