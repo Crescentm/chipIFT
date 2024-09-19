439077d9de35b554baffab497150c8ad975bfe5f
Crescentm <guyues@outlook.com>
2024-09-19 09:51:17 +0800

    refactor: refactor traverse_modify_ast function

--- src/ast_verilog.py
+++ src/ast_verilog.py
@@ -1,3 +1,4 @@
+import pprint
 from pyverilog.vparser.parser import parse
 from pyverilog.dataflow.dataflow_analyzer import VerilogDataflowAnalyzer
 from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
@@ -5,6 +6,7 @@ from pyverilog.dataflow.dataflow_codegen import VerilogCodeGenerator
 from pyverilog.dataflow.optimizer import VerilogDataflowOptimizer
 from src.utils import *
 import copy
+import inspect
 import pyverilog.dataflow.dataflow as vdfg
 import pyverilog.utils.scope as vscope
 import pyverilog.vparser.ast as vast
@@ -42,8 +44,10 @@ class DFGVerilog:
         resetedge: str = "negedge",
         searchtarget=[],
     ):
-        # Need to solve
-        # module 'pyverilog.utils.signaltype' has no attribute 'isWireArray'
+        """
+        DO NOT USE THIS !\n
+        Need to solve: module 'pyverilog.utils.signaltype' has no attribute 'isWireArray'
+        """
         optimizer = VerilogDataflowOptimizer(self.terms, self.binddict)
         optimizer.resolveConstant()
         resolved_terms = optimizer.getResolvedTerms()
@@ -61,6 +65,17 @@ class DFGVerilog:
         codegen.set_reset_info(resetname, resetedge)
         code = codegen.generateCode()
         return code
+
+    def gen_taint_vars(self):
+        taint_var = []
+        instance_dict = dict(self.instances)
+        for value in self.terms.values():
+            var_name = str(value.name[-1:])
+            module_type = instance_dict[value.name[:-1]]
+            var_type = tuple(value.termtype)
+            taint_var.append((var_name, module_type, var_type))
+
+        return taint_var
 
 
 class ASTVerilog:
@@ -89,36 +104,37 @@ class ASTVerilog:
         rslt = codegen.visit(self.source)
         return rslt
 
-    def traverse_modify_ast(self, module_index: int = 0):
-        def traverse(node):
-            children: tuple = node.children()
-            children_new = []
-            if len(children) == 0:
-                return ()
-            for child in children:
-                match type(child):
-                    case vast.Portlist | vast.Decl:
-                        ret = traverse(child)
-                        children_new.append(type(child)(ret))
-                    case vast.Ioport:
-                        children_new.append(child)
-                        taint_ioport: vast.Ioport = copy.deepcopy(child)
-                        if taint_ioport.first:
-                            taint_ioport.first.name = f"{child.first.name}_t"
-                        if taint_ioport.second:
-                            taint_ioport.second.name = f"{child.second.name}_t"
-                        children_new.append(taint_ioport)
-                    case vast.Tri | vast.Wire | vast.Reg:
-                        children_new.append(child)
-                        taint_var: vast.Reg = copy.deepcopy(child)
-                        taint_var.name = f"{taint_var.name}_t"
-                        children_new.append(taint_var)
-                    case _:
-                        children_new.append(child)
-            return tuple(children_new)
+    def traverse(self, node):
+        children: tuple = node.children()
+        children_new = []
+        if len(children) == 0:
+            return ()
+        for child in children:
+            match type(child):
+                case vast.Portlist | vast.Decl | vast.Concat:
+                    ret = self.traverse(child)
+                    children_new.append(type(child)(ret))
+                case vast.Ioport:
+                    children_new.append(child)
+                    taint_ioport: vast.Ioport = copy.deepcopy(child)
+                    # TODO: if Input/Output/Inout have nested structure
+                    if taint_ioport.first:
+                        taint_ioport.first.name = f"{child.first.name}_t"
+                    if taint_ioport.second:
+                        taint_ioport.second.name = f"{child.second.name}_t"
+                    children_new.append(taint_ioport)
+                case vast.Tri | vast.Wire | vast.Reg:
+                    children_new.append(child)
+                    taint_var: vast.Reg = copy.deepcopy(child)
+                    taint_var.name = f"{taint_var.name}_t"
+                    children_new.append(taint_var)
+                case _:
+                    children_new.append(child)
+        return tuple(children_new)
 
+    def traverse_modify_ast(self, module_index: int = 0):
         module: vast.ModuleDef = self.source.description.definitions[module_index]
-        ret = traverse(module)
+        ret = self.traverse(module)
         module_list = list(self.source.description.definitions)
         module_list[module_index] = vast.ModuleDef(module.name, ret[0], ret[1], ret[2:])  # type: ignore
         self.source.description.definitions = tuple(module_list)
@@ -151,11 +167,22 @@ def decl_add_example():
 
 
 def tarverse_example():
-    file_list = ["./verilogcode/test.v"]
+    file_list = ["./verilogcode/test4.v"]
     ast = ASTVerilog(file_list)
-    ast.traverse_modify_ast(module_index=0)
+    for i in range(ast.module_num):
+        ast.traverse_modify_ast(module_index=i)
     print(ast.gen_code())
 
 
+def dfg_example():
+    file_list = ["./verilogcode/test2.v"]
+    dfg = DFGVerilog(file_list, topmodule="top_module")
+    pprint.pprint(dfg.gen_taint_vars())
+
+
+def test():
+    print(vast.Node)
+
+
 if __name__ == "__main__":
-    tarverse_example()
+    test()
